89b570658c0435fb546430019291c346a793cf83
Aveson <avesoncarvalho25@gmail.com>
2023-10-04 18:10:10 +0100

    finished home page

---
+++ scripts/code.js
@@ -0,0 +1,6 @@
+// type writer
+const typeWriter = document.getElementById("typewriter-text");
+const text = "Lorem ipsum dolor sit amet.";
+
+typeWriter.innerHTML = text;
+typeWriter.style.setProperty("--characters", text.length);
