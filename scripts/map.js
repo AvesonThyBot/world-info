16a857c789199c3ac825246be87ebaeda39566a3
Aveson <avesoncarvalho25@gmail.com>
2023-10-09 21:11:35 +0100

    display country name based on clicked coutnry

--- scripts/map.js
+++ scripts/map.js
@@ -1,12 +1,40 @@
 import { url, options } from "./apikey.js";
 
+// function to display map and get info of map and api
 function displayMap() {
 	fetch(url, options)
 		.then((data) => data.json())
 		.then((data) => {
+			// Get name of the path element that is picked.
+			const pathElements = document.querySelectorAll("path");
+			pathElements.forEach((pathElement) => {
+				pathElement.addEventListener("click", () => {
+					let countryName = pathElement.getAttribute("name");
+
+					if (pathElement.classList.length === 0 && countryName) {
+						countryName = countryName;
+					} else {
+						countryName = pathElement.classList.value;
+					}
+					popupModal(countryName);
+				});
+			});
+
 			// check data
 			console.log(data);
 		});
+}
+
+// Modal
+function popupModal(countryName) {
+	// Modal variables
+	let countryModal = new bootstrap.Modal(document.getElementById("mapModal"));
+	const modalTitle = document.querySelector(".modal-title");
+
+	// Assign header name
+	modalTitle.textContent = `${countryName}`;
+
+	countryModal.show();
 }
 
 displayMap();
