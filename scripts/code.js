363e0d50d3167ec98158613cfd2a4a5c5ca780fa
Aveson <avesoncarvalho25@gmail.com>
2023-10-11 19:33:46 +0100

    fixed minor bug with variable

--- scripts/code.js
+++ scripts/code.js
@@ -1,3 +1,4 @@
 // type writer
-document.querySelector(".typewriter-effect").innerHTML = "This website is dedicated to displaying the statistics and weather of the world using a SVG map and Weather API.";
+const text = "This website is dedicated to displaying the statistics and weather of the world using a SVG map and Weather API.";
+document.querySelector(".typewriter-effect").innerHTML = text;
 document.querySelector(".typewriter-effect").style.setProperty("--characters", text.length);
